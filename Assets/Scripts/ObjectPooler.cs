76140acd86e4acfd4ef704843cf595a6f8a7de0f
Clownstantine <constantin2033@gmail.com>
2022-08-21 22:00:26 +0300

    Folder

---
+++ Assets/Scripts/ObjectPooler.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectPooler : MonoBehaviour
+{
+	public static ObjectPooler SharedInstance;
+	public List<GameObject> pooledObjects;
+	public GameObject objectToPool;
+	public int amountToPool;
+
+	void Awake() => SharedInstance = this;
+
+	void Start()
+	{
+		pooledObjects = new List<GameObject>();
+		for(int i = 0; i < amountToPool; i++)
+		{
+			GameObject obj = Instantiate(objectToPool);
+			obj.SetActive(false);
+			pooledObjects.Add(obj);
+			obj.transform.SetParent(transform);
+		}
+	}
+
+	public GameObject GetPooledObject()
+	{
+		for(int i = 0; i < pooledObjects.Count; i++)
+		{
+			if(!pooledObjects[i].activeInHierarchy)
+				return pooledObjects[i];
+		}
+		return null;
+	}
+}
